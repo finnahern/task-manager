a531bb2fa72133ac8593893077e6569adeb9ce50
finnahern <finna9x@gmail.com>
2021-11-19 17:20:46 +0000

    Add function to display task list to user

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // select initialisation
     var selects = document.querySelectorAll('select');
      M.FormSelect.init(selects);
-  });
+
+    // collapsible initialisation 
+    var collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
+});
 
   

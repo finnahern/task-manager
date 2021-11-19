28760cacddac4a55d99fc3eda843e22cece61a12
finnahern <finna9x@gmail.com>
2021-11-19 16:39:08 +0000

    Add form to create tasks and add them to the database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialisation
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialisation
+    var selects = document.querySelectorAll('select');
+     M.FormSelect.init(selects);
   });
+
+  
